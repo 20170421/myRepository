8b502102a093fd3eac3aae0fb1a8171075e5d21e
20170421 <j920520@gmail.com>
2018-03-11 21:47:02 +0900

    180311

--- c++/chap5/ex11.cpp
+++ c++/chap5/ex11.cpp
@@ -1,15 +1,21 @@
 #include "../../../std_lib_facilities.h"
 
+class input_error { }; // 입력받을때의 에러
+
 void get_user_input(vector<int> &input)
 {
-    int n;
+    int n, i;
 
     cout << "> ";
     cin >> n;
-    for (int i=0; n; ++i) {
+    if (!cin) // 숫자가 아닌 경우
+        throw input_error();
+    for (i=0; i<4 && n; ++i) {
         input[3-i] = n % 10;
         n /= 10;
     }
+    if (n != 0 || i < 4) // 4자리수가 아닌경우
+        throw input_error();
 }
 
 void check_bulls_and_cows(vector<int> &problem, vector<int> &input, int &bulls, int &cows)
@@ -35,9 +41,13 @@ int main()
     int bulls = 0, cows = 0;
 
     while (true) {
-        get_user_input(input);
-        check_bulls_and_cows(problem, input, bulls, cows);
-        cout << "숫소 " << bulls << "마리와 암소 " << cows << "마리\n";
+        try {
+            get_user_input(input);
+            check_bulls_and_cows(problem, input, bulls, cows);
+            cout << "숫소 " << bulls << "마리와 암소 " << cows << "마리\n";
+        } catch (input_error) {
+            error("올바른 입력값이 아닙니다.");
+        }
     }
     return 0;
 }
