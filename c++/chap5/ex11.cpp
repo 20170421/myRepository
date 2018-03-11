6887eec8dc1ed5d6c295018ca715641f0f23fbf1
20170421 <j920520@gmail.com>
2018-03-11 21:54:44 +0900

    180311

--- c++/chap5/ex11.cpp
+++ c++/chap5/ex11.cpp
@@ -1,6 +1,7 @@
 #include "../../../std_lib_facilities.h"
 
 class input_error { }; // 입력받을때의 에러
+class same_value_error { }; // 중복된값의 에러
 
 void get_user_input(vector<int> &input)
 {
@@ -13,6 +14,10 @@ void get_user_input(vector<int> &input)
     for (i=0; i<4 && n; ++i) {
         input[3-i] = n % 10;
         n /= 10;
+    }
+    for (int i=1; i<4; ++i) {
+        if (input[i-1] == input[i])
+            throw same_value_error();
     }
     if (n != 0 || i < 4) // 4자리수가 아닌경우
         throw input_error();
@@ -34,6 +39,14 @@ void check_bulls_and_cows(vector<int> &problem, vector<int> &input, int &bulls, 
     }
 }
 
+bool is_correct(vector<int> &problem, vector<int> &input) {
+    for (int i=0; i<4; ++i) {
+        if (problem[i] != input[i])
+            return false;
+    }
+    return true;
+}
+
 int main()
 {
     vector<int> problem = { 1, 2, 3, 4 };
@@ -45,8 +58,12 @@ int main()
             get_user_input(input);
             check_bulls_and_cows(problem, input, bulls, cows);
             cout << "숫소 " << bulls << "마리와 암소 " << cows << "마리\n";
+            if (is_correct(problem, input)) 
+                break;
         } catch (input_error) {
             error("올바른 입력값이 아닙니다.");
+        } catch (same_value_error) {
+            error("각 자리의 숫자는 중복될 수 없습니다.");
         }
     }
     return 0;
