8957a6488f011e4d9805e626ff3243eaac9a4f99
20170421 <j920520@gmail.com>
2018-03-15 21:19:51 +0900

    180315

--- c++/chap5/ex14.cpp
+++ c++/chap5/ex14.cpp
@@ -13,37 +13,46 @@ int main()
     vector<int> fri;
     vector<int> sat;
     vector<int> sun;
+    int mon_num, tue_num, wed_num, thu_num, fri_num, sat_num, sun_num;
 
     string day;
     int num, error_cnt = 0;
+    bool is_correct = false;
 
     while (cin) {
         cout << "> ";
-        cin >> day >> num;
+        cin >> day;
+        if (day == "|")
+            break;
+        cin >> num;
+        
         for (int i=0; i<7; ++i) {
             if (day == days1[i] || day == days2[i] || day == days3[i]) {
+                is_correct = true;
                 switch (i) {
-                    case 0: mon.push_back(num);
+                    case 0: mon.push_back(num); mon_num += num;
                         break;
-                    case 1: tue.push_back(num);
+                    case 1: tue.push_back(num); tue_num += num;
                         break;                    
-                    case 2: wed.push_back(num);
+                    case 2: wed.push_back(num); wed_num += num;
                         break;                                            
-                    case 3: thu.push_back(num);
+                    case 3: thu.push_back(num); thu_num += num;
                         break;                                        
-                    case 4: fri.push_back(num);
+                    case 4: fri.push_back(num); fri_num += num;
                         break;                                        
-                    case 5: sat.push_back(num);
+                    case 5: sat.push_back(num); sat_num += num;
                         break;                                        
-                    case 6: sun.push_back(num);
-                        break;                                        
+                    case 6: sun.push_back(num); sun_num += num;
+                        break;                           
                 }
-            } else {
-                error_cnt++;
             }
         }
+        if (is_correct == false)
+            error_cnt++;
+        is_correct = false;
     }
-    
 
+    cout << "거부된 값의 개수 : " << error_cnt << "\n";
+    
     return 0;
 }
