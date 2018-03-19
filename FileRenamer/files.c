ef5ea9aac7b577347daebd899138a2d844ca5b4c
20170421 <j920520@gmail.com>
2018-03-19 17:48:36 +0900

    180319

---
+++ FileRenamer/files.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <windows.h>
+
+#include "files.h"
+
+void print_files(char *file_path)
+{
+    HANDLE hFind;
+    WIN32_FIND_DATA FindData;
+    const char *ext = "\\*";
+
+    hFind = FindFirstFile(strcat(file_path, ext), &FindData);
+    while (FindNextFile(hFind, &FindData))
+    {
+        printf("%20.17s\t", FindData.cFileName);
+        printf("%20d\n", FindData.nFileSizeLow);
+    }
+    FindClose(hFind);
+}
---
+++ FileRenamer/files.h
@@ -0,0 +1,6 @@
+#ifndef FILES_H
+#define FILES_H
+
+void print_files(char *file_path);
+
+#endif
